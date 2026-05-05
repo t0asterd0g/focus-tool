56f497d49f4d961ef4c283cc5efd5cad21a7a527
Sunny Huynh <shuynh.design@gmail.com>
2026-05-05 15:03:47 -0700

    Proxy Supabase auth callback through app domain

--- next.config.js
+++ next.config.js
@@ -1,3 +1,12 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {}
+const nextConfig = {
+  async rewrites() {
+    return [
+      {
+        source: '/auth/v1/:path*',
+        destination: `${process.env.NEXT_PUBLIC_SUPABASE_URL}/auth/v1/:path*`,
+      },
+    ]
+  },
+}
 module.exports = nextConfig
